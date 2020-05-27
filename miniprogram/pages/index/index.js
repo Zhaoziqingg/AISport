785b79fd10ebfa96e840733cfeb34c6de0414048
Zhaoziqingg <1335850578@qq.com>
2020-05-27 22:38:49 +0800

    5/27

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -6,6 +6,51 @@ Page({
    */
   data: {
 
+    active: 0,
+    icon: {
+      normal: 'https://img.yzcdn.cn/vant/user-inactive.png',
+      active: 'https://img.yzcdn.cn/vant/user-active.png',
+    },
+    productRecommend:
+    [
+      {
+        img:"../images/index/拉伸.jpg",
+        title:"五分钟跑后拉伸"
+      },
+      {
+        img:"../images/index/瑜伽.jpg",
+        title:"瑜伽初级"
+      }   
+    ],
+   
+    comGroup:
+    [
+      {
+        img:"../images/index/拉伸组1.jpg",
+        title:"拉伸1组"
+      },
+      {
+        img:"../images/index/拉伸组2.jpg",
+        title:"拉伸2组"
+      },
+      {
+        img:"../images/index/跑步组1.jpg",
+        title:"跑步2组"
+      },
+      {
+        img:"../images/index/跑步组2.jpg",
+        title:"跑步2组"
+      },
+      {
+        img:"../images/index/拉伸组2.jpg",
+        title:"拉伸3组"
+      },
+    ],
+
+  },
+
+  onChange(event) {
+    this.setData({ active: event.detail });
   },
 
   /**
